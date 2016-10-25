065cbe209eb9b1be88825605a8e71de24ec4b493
szintia <kasziba.cintia@gmail.com>
2016-10-24 22:42:26 -0400

    add Category model object

---
+++ src/main/java/org/cintia/cbottle/web/domain/Category.java
@@ -0,0 +1,20 @@
+package org.cintia.cbottle.web.domain;
+
+public class Category {
+	
+	private String categoryId;
+	private String categoryName;
+	
+	public Category(String categoryId, String categoryName) {
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+	}
+	
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+}
