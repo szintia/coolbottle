627de28464ed98a236dde7f83cb75e37d3a0294b
szintia <kasziba.cintia@gmail.com>
2016-11-06 10:50:55 -0500

    enable autowire and component scanning

--- src/main/java/org/cintia/cbottle/web/database/dao/CustomerDAO.java
+++ src/main/java/org/cintia/cbottle/web/database/dao/CustomerDAO.java
@@ -3,10 +3,22 @@ package org.cintia.cbottle.web.database.dao;
 import java.util.List;
 
 import org.cintia.cbottle.web.database.DataAccess;
+import org.cintia.cbottle.web.database.query.CustomerQuery;
 import org.cintia.cbottle.web.domain.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomerDAO implements DataAccess<Customer> {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private RowMapper<Customer> customerRowMapper;
+	
 	@Override
 	public void insert(Customer customer) {
 		
