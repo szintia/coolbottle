7d6c40aef8c85fd5ed21aa35d46ba87cc5c487ec
szintia <kasziba.cintia@gmail.com>
2016-11-06 10:38:39 -0500

    create CustomerRowMapper to fetch the appropriate Customer record from the db

---
+++ src/main/java/org/cintia/cbottle/web/database/CustomerRowMapper.java
@@ -0,0 +1,23 @@
+package org.cintia.cbottle.web.database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.cintia.cbottle.web.domain.Customer;
+import org.springframework.jdbc.core.RowMapper;
+
+public class CustomerRowMapper implements RowMapper<Customer>{
+
+	@Override
+	public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Customer customer = new Customer();
+		customer.setEmail(rs.getString("email"));
+		customer.setPassword(rs.getString("password"));
+		customer.setName(rs.getString("customer_name"));
+		customer.setMobileNumber(rs.getString("mobileNumber"));
+		customer.setAccountBalance(rs.getBigDecimal("accountBalance"));
+		customer.setLoyalty(rs.getBoolean("loyalty"));
+		return customer;
+	}
+
+}
