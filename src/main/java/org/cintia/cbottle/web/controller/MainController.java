c6d1a4598ebe0950c37fd23c219a6f7bf335f2dd
szintia <kasziba.cintia@gmail.com>
2016-12-05 19:36:01 -0500

    get the name of the principal currently using website

--- src/main/java/org/cintia/cbottle/web/controller/MainController.java
+++ src/main/java/org/cintia/cbottle/web/controller/MainController.java
@@ -1,14 +1,26 @@
 package org.cintia.cbottle.web.controller;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 	
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public String getHomePage() {
-		return "home";
+	private static final String HOME_MAPPING = "/home";
+	private static final String HOME_VIEW = "home";
+	
+	@RequestMapping(value = HOME_MAPPING, method = RequestMethod.GET)
+	public String getHomePage(Model model, HttpServletRequest request) {
+		Principal user = request.getUserPrincipal();
+		if(user != null) {
+			model.addAttribute("customerName", user.getName());
+		}
+		return HOME_VIEW;
 	}
 }
