df1dc787ec0796804b987645284bbbf20664601e
szintia <kasziba.cintia@gmail.com>
2017-01-08 12:02:18 -0500

    implement product specific queries

--- src/main/java/org/cintia/cbottle/web/database/query/BottleQuery.java
+++ src/main/java/org/cintia/cbottle/web/database/query/BottleQuery.java
@@ -1,5 +1,31 @@
 package org.cintia.cbottle.web.database.query;
 
-public enum BottleQuery implements Query{
+import java.text.DateFormat;
+import java.util.Date;
 
+import org.cintia.cbottle.web.domain.Bottle;
+
+public enum BottleQuery implements Query {
+	
+	INSERT("INSERT INTO product (product_id, category_id, product_name, description, price, amount, date) VALUES (?, ?, ?, ?, ?, ?,TO_DATE( ? , 'YYYY-MM-DD'))"),
+	SELECT_ALL("SELECT * FROM product"),
+	DELETE("DELETE FROM product WHERE product_id = ?");
+
+	private String query;
+	private static final Date date = new Date();
+	
+	private BottleQuery(String query) {
+		this.query = query;
+	}
+	
+	@Override
+	public String sql() {
+		return query;
+	}
+	
+	public static Object[] mapInsertParams(Bottle bottle) {
+		String sqlDate = DateFormat.getInstance().format(date);
+		Object[] valueParams = {bottle.getProductId(), bottle.getCategoryId(), bottle.getDescription(), bottle.getPrice(), bottle.getAmount(), sqlDate};
+		return valueParams;
+	}
 }
